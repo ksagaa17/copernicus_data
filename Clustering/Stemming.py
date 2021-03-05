268eb78efbaa35d6317fd39c69092d5f4f29809b
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-03-05 16:44:38 +0100

    Added Examples

---
+++ Clustering/Stemming.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import nltk
+import re
+import os
+import codecs
+from sklearn import feature_extraction
+
+
+stopwords = nltk.corpus.stopwords.words('english')
+
+def Stemming(stopwords, amount = 10):
+    x = 1
+    return x
+
+
+
+def remove_stops():
+    x = 2
+    return x
+
+
+#%%
+#nltk.download() # run to download a corpus
+stopwords = nltk.corpus.stopwords.words('english')
+#print(stopwords[:10])
---
+++ Clustering/StopWords_example.py
@@ -0,0 +1,22 @@
+from nltk.corpus import stopwords  
+from nltk.tokenize import word_tokenize  
+
+#nltk.download() # run to download a corpus  (Download punkt and stopwords from all packages)
+example_sent = """This is a sample sentence, 
+                  showing off the stop words filtration."""
+  
+stop_words = set(stopwords.words('english'))  
+  
+word_tokens = word_tokenize(example_sent)  
+  
+filtered_sentence = [w for w in word_tokens if not w in stop_words]  
+  
+filtered_sentence = []  
+  
+for w in word_tokens:  
+    if w not in stop_words:  
+        filtered_sentence.append(w)  
+  
+print(word_tokens)  
+print(filtered_sentence)  
+
