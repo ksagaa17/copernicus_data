c915f03534d781305d535e64bfdc42ae33e15690
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-03-06 10:58:30 +0100

    added Stemming and removing stop words

--- Clustering/Modules.py
+++ Clustering/Modules.py
@@ -3,17 +3,27 @@ This script contains functions for tokenisation, stemming and removing stop word
 used as preprocessing for the clustering process.
 """
 
-import numpy as np
-import pandas as pd
+
 import nltk
 import re
-import os
-import codecs
-from sklearn import feature_extraction
 import json
 
 
 def Tokenize(text):
+    """
+    Seperates a sentence into tokens
+
+    Parameters
+    ----------
+    text : string
+        Sentence to by tokenized 
+
+    Returns
+    -------
+    filtered_tokens : list
+        Contains a token in each entrance.
+
+    """
     # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
     tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
     filtered_tokens = []
@@ -24,14 +34,54 @@ def Tokenize(text):
     return filtered_tokens
 
 
-def Stemming(text):
-    x = 1
-    return x
+def Stemming(tokens, stemmer = nltk.stem.snowball.SnowballStemmer("english")):
+    """
+    Stems words
+
+    Parameters
+    ----------
+    tokens : list of strings containing a single word
+        Strings to be stemmed 
+
+    stemmer : nltk object
+        Stemming technique
+        
+    Returns
+    -------
+    stems : list
+        Contains a stemmed token in each entrance.
+
+    """
+    filtered_tokens = []
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    stems = [stemmer.stem(t) for t in filtered_tokens]
+    return stems
 
 
-def remove_stops(text, stopwords):
-    x = 1
-    return x
+def remove_stops(stems, stopwords, amount = 179):
+    """
+    Removes Stopwords
+
+    Parameters
+    ----------
+    stems : list of strings containing a single stemmed word
+        Strings with words which might be deleted 
+
+    stopwords : list of stopwods
+        The words which are unwanted
+    
+    amount : interger
+         the amount of stopwords to be removed   
+    Returns
+    -------
+    filtered_sentence : list
+        Containing no stop words.
+
+    """
+    filtered_sentence = [w for w in stems if not w in stopwords[:amount]] 
+    return filtered_sentence
 
 
 #%% #Testing the modules
@@ -44,15 +94,18 @@ for i in range(N):
     tmp = json.dumps(text[i])
     text_string.append(tmp)
 
-#nltk.download() # run to download nltk dependencies
+# nltk.download() # run to download nltk dependencies
 
-#Tokenisation
-tokenised = Tokenize(text_string[0])    
+# Tokenisation
+tokens = Tokenize(text_string[0])    
+print(tokens)
 
-# stemming
+# Stemming
 stemmer = nltk.stem.snowball.SnowballStemmer("english")
+stems = Stemming(tokens, stemmer = stemmer)
+print(stems) # for instance look at entrace 9, 10, 11 or 12
 
-#Remove stopwords
+# Remove stopwords
 stopwords = nltk.corpus.stopwords.words('english')
-#print(stopwords[:10])
-
+no_stops = remove_stops(stems, stopwords, amount = 179) # for instance look at entrace 18 or 19 compared to stems
+print(no_stops)
