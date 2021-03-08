95a50b44d38eaa3e88bd1706dd9ec95ee89e444a
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-03-08 17:08:02 +0100

    Update Jaccard_copernicus.py

--- Clustering/Jaccard_copernicus.py
+++ Clustering/Jaccard_copernicus.py
@@ -1,8 +1,11 @@
 import Modules as M
 import json
+import os
 
+#%%CDS
+pardir = os.path.dirname(os.getcwd())
 
-with open('copernicus_data/copernicus_scrape/CDS_data.json') as f:
+with open(pardir+'/copernicus_scrape/CDS_data.json') as f:
   CDS = json.load(f)
 
 CDS_string = []
@@ -10,19 +13,22 @@ N = len(CDS)
 for i in range(N):
     tmp = json.dumps(CDS[i])
     CDS_string.append(tmp)
+    
+CDS_string_preprocessed = M.Preprocessing(CDS_string)
+Jaccard_matrix_CDS = M.jaccard_matrix(CDS_string_preprocessed)
+near_CDS = M.nearest_docs(CDS, Jaccard_matrix_CDS, 95, 10)    
 
-with open('copernuicus_data/copernicus_scrape/ADS_data.json') as f:
+#%%ADS
+pardir = os.path.dirname(os.getcwd())
+
+with open(pardir+'copernicus_scrape/ADS_data.json') as f:
   ADS = json.load(f)
 
 ADS_string = []
 K = len(ADS)
 for i in range(K):
-    tmp = json.dumps(ADS[i])
     ADS_string.append(tmp)
 
 ADS_string_preprocessed = M.Preprocessing(ADS_string)
 Jaccard_matrix_ADS = M.jaccard_matrix(ADS_string_preprocessed) 
-
-CDS_string_preprocessed = M.Preprocessing(CDS_string)
-Jaccard_matrix_CDS = M.jaccard_matrix(CDS_string_preprocessed)
- 
+near_ADS = M.nearest_docs(ADS, Jaccard_matrix_ADS, 2, 2)
