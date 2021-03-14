2f2722ce9707d9dda1cb729db67e9facb1c015f6
ksagaa17 <kristiansolvang@live.dk>
2021-03-14 12:41:31 +0100

    Changed ADSDataSpider.py

--- copernicus_scrape/copernicus_scrape/spiders/ADSDataSpider.py
+++ copernicus_scrape/copernicus_scrape/spiders/ADSDataSpider.py
@@ -1,6 +1,6 @@
 """
 This script contains spiders for collecting the title, description and
-parameters for a dataset on the Copernicus database 
+parameters for a dataset on the Copernicus Atmosphere Data Store database 
 an .json file is returned with the entities for a given website.
 """
 
@@ -14,15 +14,15 @@ from logzero import logfile, logger
 import json
 
 
-class DataSpider(scrapy.Spider):
+class ADSDataSpider(scrapy.Spider):
     """
-    Scrapes: https://ads.atmosphere.copernicus.eu/cdsapp#!/dataset/cams-global-reanalysis-eac4-monthly?tab=overview
+    Scrapes: https://ads.atmosphere.copernicus.eu/cdsapp#!/search?type=dataset
     And returns a .json file with the title, description and parameters of the page
-    Run this spider with: /main/copernicus_scrape$ scrapy crawl ScrapeSpider -o ScrapeSpider.json 
+    Run this spider with: /main/copernicus_scrape$ scrapy crawl ADSDataScrapeSpider -o data/ADS_data.json 
     """
     # Initializing log file
     logfile("DataScrapeSpider.log", maxBytes=1e6, backupCount=3)
-    name = "DataScrapeSpider"
+    name = "ADSDataScrapeSpider"
     allowed_domains = ["toscrape.com"]
 
     # Using a dummy website to start scrapy request
@@ -32,18 +32,21 @@ class DataSpider(scrapy.Spider):
 
     def parse_urls(self, response):
         #Use headless option to not open a new browser window
+
+        # FIREFOX
         # options = webdriver.FirefoxOptions()
         # options.add_argument("headless")
         # desired_capabilities = options.to_capabilities()
         # driver = webdriver.Firefox(desired_capabilities=desired_capabilities)
         
+        # CHROME
         options = webdriver.ChromeOptions()
         options.add_argument("headless")
         desired_capabilities = options.to_capabilities()
         driver = webdriver.Chrome(desired_capabilities=desired_capabilities)
         
-        
-        with open("ADS_dataset_urls.json", "r") as f:
+        # Get urls
+        with open("data/ADS_dataset_urls.json", "r") as f:
             temp_list = json.load(f)
 
         url_list = list(map(lambda x: x["url"], temp_list))
@@ -73,7 +76,7 @@ class DataSpider(scrapy.Spider):
             wait = WebDriverWait(driver, 5)
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "abstract-text")))
         
-            # # Description
+            # Description
             descriptions = driver.find_elements_by_class_name("abstract-text")
             description_count = 0
             tmp1 = []
