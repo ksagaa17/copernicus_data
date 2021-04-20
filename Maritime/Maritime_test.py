3bf2db9777df02946148cb39fce487faad76a885
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-04-20 16:45:08 +0200

    Create Maritime_test.py

---
+++ Maritime/Maritime_test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("Maritime/tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
+              'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
+
+
+
+
