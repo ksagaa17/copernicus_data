b022ac63bf25550eb03873557e92b7935506f7e5
ksagaa17 <kristiansolvang@live.dk>
2021-04-25 18:00:31 +0200

    Update grand_error.py

--- Maritime/grand_error.py
+++ Maritime/grand_error.py
@@ -7,18 +7,21 @@ Created on Sat Apr 24 13:53:04 2021
 
 import numpy as np
 import pandas as pd
-import time_func1 as tf
+import time_func as tf
 import extract_function as clean
 import matplotlib.pyplot as plt
 
-def grand_error(df, plot_hist=False):
+def grand_error(df, percent=1, plot_hist=False):
     """
     Computes the mean and standard diviation of the absolute error of the etas.
 
     Parameters
     ----------
     df : panda dataframe
-        Uncleaned Log fra ETA1..
+        cleaned Log fra ETA1.
+    percent : float, optional
+        Number specifiyng the percent of data we want to use in the calculations
+        must be between 0 and 1
     plot_hist : boolean, optional
         Whether to show histograms of absolute error. The default is False.
 
@@ -34,7 +37,7 @@ def grand_error(df, plot_hist=False):
         Standard deviation of absolute error of eta_erp.
 
     """
-    df = clean.clean_data(df)
+    
     track_ids = df.track_id.unique()   
     
     abs_error_erp = np.array([])
@@ -57,7 +60,11 @@ def grand_error(df, plot_hist=False):
         
     abs_error_erp = abs_error_erp/3600       # in hours
     abs_error_ais = abs_error_ais/3600       # in hours
-
+    
+    print(len(abs_error_ais))
+    abs_error_ais = np.sort(abs_error_ais)[:int(percent*len(abs_error_ais))]
+    print(len(abs_error_ais))
+          
     mae_erp = np.mean(abs_error_erp)
     mae_ais = np.mean(abs_error_ais)
     
@@ -68,26 +75,57 @@ def grand_error(df, plot_hist=False):
         plt.figure()
         plt.hist(abs_error_ais)
         plt.show()
+        
+        plt.figure()
+        plt.hist(abs_error_erp)
+        plt.show()
     
     return mae_ais, std_ais, mae_erp, std_erp
-    
+
 
 if __name__ == "__main__":
+    # =========================================================================
+    #     Compute grand mean and standard deviation using all relavant data
+    # =========================================================================
     df = pd.read_csv("data/tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
     df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
                   'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
+
+    df = clean.clean_data(df)
+
+    mae_ais, std_ais, mae_erp, std_erp = grand_error(df, percent=0.5, plot_hist=False)
     
-    mae_ais, std_ais, mae_erp, std_erp = grand_error(df, plot_hist=True)
     
+    # #%%
+    # # =========================================================================
+    # #     Compute grand mean for data before Suez Crisis (before 2021-03-23)
+    # # =========================================================================
+    # df = pd.read_csv("data/tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+    # df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
+    #               'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
     
+    # stamps = df['ata_ais'].to_numpy().astype(np.datetime64)
+    # df = df.loc[stamps <= np.datetime64('2021-03-23T00:00')]
     
     # df = clean.clean_data(df)
+
+    # mae_ais, std_ais, mae_erp, std_erp = grand_error(df, plot_hist=False)
     
-    # track_ids = df.track_id.unique()
+    # #%%
+    # # =========================================================================
+    # #     Compute grand mean for data after Suez Crisis (after 2021-03-23)
+    # # =========================================================================
+    # df = pd.read_csv("data/tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+    # df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
+    #               'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
     
-    # liste = []
-    # for tr in track_ids:
-    #     liste.append(len(tf.ata_Extract(df, tr)))
+    # stamps = df['stamp'].to_numpy().astype(np.datetime64)
+    # df = df.loc[stamps >= np.datetime64('2021-03-23T00:00')]
+    
+    # df = clean.clean_data(df)
+
+    # mae_ais, std_ais, mae_erp, std_erp = grand_error(df, plot_hist=False)
+    
     
     
 
