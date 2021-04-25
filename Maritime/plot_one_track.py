158821efb300826411dbbcfc163eb9df095d9281
ksagaa17 <kristiansolvang@live.dk>
2021-04-25 17:08:30 +0200

    added 100 plots

--- Maritime/plot_one_track.py
+++ Maritime/plot_one_track.py
@@ -8,14 +8,12 @@ Created on Sat Apr 24 15:32:21 2021
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import time_func as tf
 import extract_function as clean
 from datetime import datetime
 
 def eta_extract_with_time(df, track_id):
-    #indexes = df.query('track_id == {0}'.format(track_id)).index
-    
-    # Virker ikke endnu, mangler at virker for andet en første track id
     
     df = df[df['track_id'] == track_id]
     
@@ -25,12 +23,9 @@ def eta_extract_with_time(df, track_id):
     
     idx = np.where((eta_erp != 'nan') & (eta_ais != 'nan'))
     
-    eta_erp = eta_erp.loc[idx]
-    eta_ais = eta_ais.loc[idx]
-    stamp = stamp.loc[idx]
-    
-
-    
+    eta_erp = eta_erp[idx]
+    eta_ais = eta_ais[idx]
+    stamp = stamp[idx]
     
     return eta_erp, eta_ais, stamp
 
@@ -39,28 +34,44 @@ def plot_eta_track(df, track_id):
     ata_ais = tf.ata_Extract(df, track_id)[0]
     
     FMT = '%Y-%m-%d %H:%M:%S'
-    eta_erp_list = []
-    eta_ais_list = []
     stamp_list = []
     
     ata_ais = datetime.strptime(ata_ais, FMT)
+    eta_erp = eta_erp.astype(np.datetime64) 
+    eta_ais = eta_ais.astype(np.datetime64)
+    
     
     for i in range(len(stamp)):
-        eta_erp_list.append(datetime.strptime(eta_erp[i], FMT))
-        eta_ais_list.append(datetime.strptime(eta_ais[i], FMT))
         time_before = ata_ais - datetime.strptime(stamp[i], FMT)
         stamp_list.append((time_before.days * 24 * 3600 + time_before.seconds)/3600)
         
-        
-    plt.figure()
-    plt.plot(stamp_list, eta_erp_list, label='eta_erp')
-    plt.plot(stamp_list, eta_ais_list, label='eta_ais')
-    plt.hlines(ata_ais, min(stamp_list), max(stamp_list), color='k', label='ata_ais')
-    plt.xlim(max(stamp_list), min(stamp_list))
-    plt.legend()
-    plt.xlabel('Hours before arrival')
-    plt.ylabel('Date and hour')
-    plt.show()
+    
+    ylim_lower = np.min([eta_erp, eta_ais])
+    ylim_upper = np.max([eta_erp, eta_ais])
+    yrange = (ylim_upper - ylim_lower).astype('timedelta64[m]')
+    ylims = (np.min([eta_erp, eta_ais]) - 0.05*yrange, 
+             np.max([eta_erp, eta_ais]) + 0.05*yrange)
+    
+    xlim_lower = max(stamp_list)
+    xlim_upper = min(stamp_list)
+    xrange = xlim_lower - xlim_upper
+    xlims = (xlim_lower + 0.02*xrange, xlim_upper - 0.02*xrange)
+    
+    
+    fig, ax = plt.subplots()
+    ax.plot(stamp_list, eta_erp, label='eta_erp')
+    ax.plot(stamp_list, eta_ais, label='eta_ais')
+    ax.hlines(ata_ais, min(stamp_list), max(stamp_list), color='k', label='ata_ais')
+    ax.set_xlim(xlims)
+    ax.set_ylim(ylims)
+    ax.yaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
+    ax.legend()
+    ax.set_xlabel('Hours before arrival')
+    ax.set_ylabel('Date and time')
+    ax.set_title('ETA for track id: {}'.format(track_id))
+    plt.subplots_adjust(left=.2)
+    plt.savefig('track_plots/{}.png'.format(track_id))
+    #plt.show()
 
 df = pd.read_csv("data/tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
 df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
@@ -69,5 +80,5 @@ df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
 df = clean.clean_data(df)
 track_ids = df.track_id.unique()
 
-for track_id in track_ids[:1]:
+for track_id in track_ids[:100]:
     plot_eta_track(df, track_id)
