773d4f6c1db865f028683aab1a148d1a7a6ad7f0
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-04-27 16:28:00 +0200

    er i gang

--- Maritime/hourly_diff_new.py
+++ Maritime/hourly_diff_new.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 # Load data
 pardir = os.path.dirname(os.getcwd())
-df = pd.read_csv(pardir + "\\Maritime\\data\\tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+#df = pd.read_csv(pardir + "\\Maritime\\data\\tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+df = pd.read_csv("Maritime\\data\\tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
 df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
               'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
     
