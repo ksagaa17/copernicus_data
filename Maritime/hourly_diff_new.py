27efee0b470f786cc8454fe26c98729ed8361d98
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2021-04-27 16:07:44 +0200

    Create hourly_diff_new.py

---
+++ Maritime/hourly_diff_new.py
@@ -0,0 +1,15 @@
+import utillities as ut
+import os
+import pandas as pd
+
+
+# Load data
+pardir = os.path.dirname(os.getcwd())
+df = pd.read_csv(pardir + "\\Maritime\\data\\tbl_ship_arrivals_log_202103.log", sep = "|", header=None)
+df.columns = ['track_id', 'mmsi', 'status', 'port_id', 'shape_id', 'stamp',
+              'eta_erp', 'eta_ais', 'ata_ais', 'bs_ts', 'sog', 'username']
+    
+df = ut.clean_data(df)
+df = ut.add_hours_bef_arr(df)
+
+
